cdecc739eb5dd07cfe09d14f4dbc649304508ea9
Puttzer <53831562+Puttzer@users.noreply.github.com>
2020-03-09 15:52:00 +0100

    Gallery Gang helpless yet again!

---
+++ src/components/Lightbox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="lightbox" v-if="show == true"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: Boolean
+  },
+  data() {
+    return {
+      images: [
+        {
+          url:
+            "https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&&fm=jpg&w=400&fit=max",
+          id: 1
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.lightbox {
+  //   background-color: #071619;
+  background-color: black;
+}
+</style>
