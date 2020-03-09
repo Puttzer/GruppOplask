4562f08733ed8759c42d5ef17e09f301179328b8
Artur <Artur.Hoppner@hotmail.com>
2020-03-09 12:36:01 +0100

    URL
    Set URL and fetch (gonna change it to home)

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,6 @@
 <template>
-<section id="app">
+<section>
+<input class="searchstyle" type="text" v-model="search" v-on:keyup.enter="submit">
 
 </section>
 
@@ -9,11 +10,42 @@
 
 
 export default {
-// components: { xxx },
-data:() => ({}),
+    name: "Search",
+data:() => ({
 
-methods:{}
+return: {
+
+    images: [],
+    baseurl: `https://api.unsplash.com/search/photos?page=1&query=office&client_id=xIJxifBLKfQ8_NIf_DtYTV2PDygh-oXGyu7ki-qQ-Uk`,
+    search: ''
+}
+
+    }),
+
+methods:{
+
+submit(){
+const url = `https://api.unsplash.com/search/photos?page=1&query=${this.search}&client_id=xIJxifBLKfQ8_NIf_DtYTV2PDygh-oXGyu7ki-qQ-Uk`
+        
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function(data) {
+            let images = data.results;
+            this.$emit("search", images)
+
+            })
+            .catch(function(error) {
+                console.log(error);
+         })
+        }
+    }
 }
 
 </script>
 
+<style lang="scoped">
+.searchstyle {
+    border-radius: 60%;
+}
+
+</style>
